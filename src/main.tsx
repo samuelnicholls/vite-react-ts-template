f6bba8579324e5b039552d2199d7ade91952575b
Samuel Nicholls <samuel.nicholls@hey.com>
2024-12-06 09:06:19 +0000

    Adding in biome js

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const element = document.getElementById("root");
+if (element === null) throw new Error("Root container missing in index.html");
+
+createRoot(element).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
