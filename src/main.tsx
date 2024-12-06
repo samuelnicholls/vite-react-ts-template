db26c6d041e41e74418df8b7136aca856409855f
Samuel Nicholls <samuel.nicholls@hey.com>
2024-12-06 09:14:46 +0000

    Adding in tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { App } from "./App.tsx";
 import "./index.css";
-import App from "./App.tsx";
 
 const element = document.getElementById("root");
 if (element === null) throw new Error("Root container missing in index.html");
